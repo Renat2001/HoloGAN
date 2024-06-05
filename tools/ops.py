9fe16a6c72dc1a9d7d27d0835577a9f205dca1a3
Renat Abdrakhmanov <61690545+Renat2001@users.noreply.github.com>
2024-06-05 12:02:52 +0000

    Replace merge with merge_all

--- tools/ops.py
+++ tools/ops.py
@@ -11,7 +11,7 @@ except:
   image_summary = tf.summary.image
   scalar_summary = tf.summary.scalar
   histogram_summary = tf.summary.histogram
-  merge_summary = tf.summary.merge
+  merge_summary = tf.summary.merge_all
   SummaryWriter = tf.summary.FileWriter
 
 def sigmoid_cross_entropy_with_logits(x, y):
